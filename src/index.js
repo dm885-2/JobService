7b0ac340451b45cccbf55a21fa8003d6d348288f
Michael <clonex_kontakt@hotmail.com>
2021-12-08 08:59:50 +0100

    Added routes needed for service

--- src/index.js
+++ src/index.js
@@ -1,15 +1,28 @@
-import {host, getTokenData, subscriber} from "./helpers.js";
+import {host, query, subscriber} from "./helpers.js";
 
-// Example
-export async function ping(msg, publish){
-    const isLoggedIn = await getTokenData(msg.token);
+export async function addJob(msg, publish){
+}
 
-    publish("pong", {
-        check: isLoggedIn ? true : false
+export async function queueCheck(msg, publish){
+    const queue = await query("SELECT * FROM `jobs` WHERE `status` = '0' ORDER BY `id` ASC LIMIT 1");
+    if(queue)
+    {
+
+    }
+}
+
+export async function jobHistory(msg, publish){
+    const data = await query("SELECT * FROM `jobs` WHERE `user` = ? ORDER BY `id` DESC LIMIT 50", [
+        msg.userID // Should be token userID?
+    ]);
+    publish("job-history-response", {
+        data: data || [],
     });
 }
 
 if(process.env.RAPID)
 {
-    subscriber(host, [{river: "template", event: "ping", work: ping}]);
+    subscriber(host, [{river: "jobs", event: "add-job", work: addJob}]); // Adds a new job
+    subscriber(host, [{river: "jobs", event: "queue-check", work: queueCheck}]); // Runs the next job in the queue, if there is any
+    subscriber(host, [{river: "jobs", event: "job-history", work: jobHistory}]); // Gets the job history of a user
 }
